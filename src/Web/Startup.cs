6f44144458285f9922f9308648782a76136a16c8
Anton <antongunko@outlook.com>
2021-01-04 21:02:44 +0300

    add logger

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -11,10 +11,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Web.Logger;
 
 namespace Web
 {
@@ -36,8 +39,16 @@ namespace Web
             services.AddDbContext<IdentityContext>(options =>
              options.UseSqlServer(Configuration.GetConnectionString("IdentityConnection")));
 
-            services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<IdentityContext>();
+            services.AddIdentity<ApplicationUser, IdentityRole>(
+                  opts =>
+                  {
+                      opts.Password.RequiredLength = 6;
+                      opts.Password.RequireNonAlphanumeric = false;
+                      opts.Password.RequireLowercase = false;
+                      opts.Password.RequireUppercase = false;
+                      opts.Password.RequireDigit = false;
+                  })
+                  .AddEntityFrameworkStores<IdentityContext>();
 
             services.AddTransient<IUnitOfWork, EFUnitOfWork>();
 
@@ -45,8 +56,14 @@ namespace Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+
+            loggerFactory.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logger.txt"));
+            var logger = loggerFactory.CreateLogger("FileLogger");
+
+            env.EnvironmentName = "Production";
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
