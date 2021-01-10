662aea8e6a674ca6c3289d158a6c64218e5e2e38
Anton <antongunko@outlook.com>
2021-01-10 19:27:54 +0300

    upd annotations view models

--- src/Web/Models/Dish/AddDishViewModel.cs
+++ src/Web/Models/Dish/AddDishViewModel.cs
@@ -6,21 +6,22 @@ namespace Web.Models.Dish
     {
         public int CatalogId { get; set; }
 
-        [Required(ErrorMessage = "NameNotSpecified")]
+        [Required(ErrorMessage = "Enter name")]
         [Display(Name = "Name")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "InfoNotSpecified")]
+        [Required(ErrorMessage = "Enter info")]
         [Display(Name = "Info")]
         public string Info { get; set; }
 
-        [Required(ErrorMessage = "WeightNotSpecified")]
+        [Required(ErrorMessage = "Enter weight")]
         [Display(Name = "Weight")]
         public double Weight { get; set; }
 
-        [Required(ErrorMessage = "PriceNotSpecified")]
+        [Required(ErrorMessage = "Enter price")]
         [Display(Name = "Price")]
         public decimal Price { get; set; }
+
         public string Path { get; set; }
     }
 }
--- src/Web/Models/Dish/SortState.cs
+++ src/Web/Models/Dish/SortState.cs
@@ -3,7 +3,7 @@ namespace Web.Models.Dish
 {
     public enum SortState
     {
-         PriceAsc,    // по цене по возрастанию
-        PriceDesc   // по цене по убыванию
+         PriceAsc,    
+        PriceDesc 
     }
 }
