cf420c08d2bc9e4955fac99376865350bf7f2ee0
Anton <antongunko@outlook.com>
2021-01-12 02:54:20 +0300

    add report mvc

---
+++ src/Core/Constants/ReportConstants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Constants
+{
+    public static class ReportConstants
+    {
+        public const string REPORT_BY_PROVIDER = "Report by provider";
+        public const string REPORT_BY_PROVIDERS_PER = "Report by providers per";
+        public const string REPORT_BY_PROVIDERS_WITH= "Report by providers with";
+        public const string REPORT_BY_PROVIDERS_FOR_ALL_TIME = "Report by providers for all time";
+
+        public const string REPORT_BY_USER = "Report by user";
+        public const string REPORT_BY_USERS_PER = "Report by users per";
+        public const string REPORT_BY_USERS_WITH = "Report by users with";
+        public const string REPORT_BY_USERS_FOR_ALL_TIME = "Report by users for all time";
+
+
+        public const string PER = "per";
+        public const string WITH = "with";
+        public const string BY = "by";
+        public const string FOR_ALL_TIME  = "for all time";
+    }
+}
