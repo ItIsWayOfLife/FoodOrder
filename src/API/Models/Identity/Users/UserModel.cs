9b1c9ac95ae95337ab56d77230f1152b0ee24d81
Anton <antongunko@outlook.com>
2021-01-16 01:48:48 +0300

    Add annotations user models

--- src/API/Models/Identity/Users/UserModel.cs
+++ src/API/Models/Identity/Users/UserModel.cs
@@ -1,13 +1,24 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace API.Models.Identity.Users
 {
     public class UserModel
     {
         public string Id { get; set; }
+
+        [Required(ErrorMessage = "Enter email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter First Name")]
         public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Enter Second Name")]
         public string Lastname { get; set; }
+
         public string Patronymic { get; set; }
+
+        [Required(ErrorMessage = "Enter password")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
--- src/API/Models/Identity/Users/UserModelChangePasword.cs
+++ src/API/Models/Identity/Users/UserModelChangePasword.cs
@@ -1,10 +1,18 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace API.Models.Identity.Users
 {
     public class UserModelChangePasword
     {
+        [Required(ErrorMessage = "Enter id")]
         public string Id { get; set; }
+
+        [Required(ErrorMessage = "Enter new password")]
+        [DataType(DataType.Password)]
         public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Enter old password")]
+        [DataType(DataType.Password)]
         public string OldPassword { get; set; }
     }
 }
