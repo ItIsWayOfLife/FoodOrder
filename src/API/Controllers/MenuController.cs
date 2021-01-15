775a8eef857f65e9c64011118a0528ab062c799e
Anton <antongunko@outlook.com>
2021-01-15 13:21:14 +0300

    Add Swagger

---
+++ src/API/Controllers/MenuController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+
+
+    public class Menu
+    {   /// <summary>
+    /// Id menu
+    /// </summary>
+        public string Id {get;set;}
+
+        /// <summary>
+        /// Name menu
+        /// </summary>
+        public string Name { get; set; }
+        }
+    /// <summary>
+    /// Menu controller 
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+            List<Menu> menus = new List<Menu>() {
+         new Menu() { Id ="1" , Name = "Menu1"},
+           new Menu() { Id ="2" , Name = "Menu2"},
+             new Menu() { Id ="3" , Name = "Menu3"}
+        };
+
+        /// <summary>
+        /// Get menus
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            return new ObjectResult(menus);
+        }
+
+        /// <summary>
+        /// Add menu
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns>List menus</returns>
+        [HttpPost]
+        public IActionResult Post(Menu item)
+        {
+            menus.Add(item);
+
+            return Ok(item);
+        }
+    }
+}
--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -14,7 +14,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace API
@@ -65,6 +67,21 @@ namespace API
             });
 
             services.AddControllers();
+
+            services.AddSwaggerGen(options => 
+            {
+                options.SwaggerDoc("v1",
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = "Swagger test API",
+                        Description = "Test API",
+                        Version = "v1"
+                    });
+
+                var fileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var filePath = Path.Combine(AppContext.BaseDirectory, fileName);
+                options.IncludeXmlComments(filePath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -90,6 +107,14 @@ namespace API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options => 
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger test API");
+                options.RoutePrefix = "";
             });
         }
     }
