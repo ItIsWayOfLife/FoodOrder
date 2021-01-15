efc4bfcea11823c2a411ce1569a8e077a0a56806
Anton <antongunko@outlook.com>
2021-01-16 02:40:41 +0300

    Add UploadController

---
+++ src/API/Controllers/UploadController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "admin")]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost, DisableRequestSizeLimit]
+        public IActionResult Upload()
+        {
+            var file = Request.Form.Files[0];
+            var folderName = Path.Combine("wwwroot", "files", "images");
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+            if (file.Length > 0)
+            {
+                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                var fullPath = Path.Combine(pathToSave, fileName);
+
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                return Ok(new { fileName });
+            }
+            else
+            {
+                return BadRequest("Invalid file");
+            }
+        }
+    }
+}
