962fe2ee737b8c23c0040577a682f803c1d115e5
Anton <antongunko@outlook.com>
2021-01-16 02:31:21 +0300

    Add annotations provider models

--- src/API/Models/Provider/ProviderModel.cs
+++ src/API/Models/Provider/ProviderModel.cs
@@ -1,12 +1,22 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Models.Provider
 {
     public class ProviderModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter time work with")]
         public string TimeWorkWith { get; set; }
+
+        [Required(ErrorMessage = "Enter time work to")]
         public string TimeWorkTo { get; set; }
         public bool IsActive { get; set; }
         public bool IsFavorite { get; set; }
