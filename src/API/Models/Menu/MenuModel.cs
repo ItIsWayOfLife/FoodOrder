27f8b63574cbab37c8def382514038aac996b281
Anton <antongunko@outlook.com>
2021-01-16 10:39:01 +0300

    Add annotations menu model

--- src/API/Models/Menu/MenuModel.cs
+++ src/API/Models/Menu/MenuModel.cs
@@ -1,10 +1,15 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace API.Models.Menu
 {
     public class MenuModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Enter info")]
         public string Info { get; set; }
+
+        [Required(ErrorMessage = "Enter date")]
         public string Date { get; set; }
         public int ProviderId { get; set; }
     }
