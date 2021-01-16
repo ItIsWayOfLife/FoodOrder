5a021c2da88770fda39efb32eaa43d696ee84942
Anton <antongunko@outlook.com>
2021-01-16 13:21:53 +0300

    Add Logger

--- src/API/Startup.cs
+++ src/API/Startup.cs
@@ -2,6 +2,7 @@ using API.Configurators;
 using API.Exceptions;
 using API.Helpers;
 using API.Interfaces;
+using API.Logger;
 using Core.Identity;
 using Core.Interfaces;
 using Core.Services;
@@ -134,8 +135,11 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logger.txt"));
+            var logger = loggerFactory.CreateLogger("FileLogger");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
